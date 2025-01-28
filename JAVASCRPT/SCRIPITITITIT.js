525116fb0bd771c384ffc7b8ad29391398910ddb
VANIA LA MAS PRO <vaniachmoy@gmail.com>
2025-01-28 13:43:42 -0600

    CSS
    se agrego carpeta de estilos css

---
+++ JAVASCRPT/SCRIPITITITIT.js
@@ -0,0 +1,20 @@
+const boton=document.getElementById('edgarbebesin')
+boton.addEventListener('click', function(){
+    alert("Me gustas mucho edgar, te quieres casar with me?");
+});
+const boton2=document.getElementById('no')
+boton2.addEventListener('click',() => {
+    const windowWidth= window.innerWidth;
+    const windowHeight= window.innerHeight;
+
+    const newX=Math.random()*(windowWidth-boton2.offsetWidth);
+    const newY=Math.random()*(windowHeight-boton2.offsetHeight);
+
+    boton2.style.left= `${newX}px`;
+    boton2.style.top=`${newY}px`;
+});
+
+const boton3=document.getElementById('si')
+boton3.addEventListener('click', function(){
+    alert("SIIII YO TAMBIEN QUIERO");
+});
